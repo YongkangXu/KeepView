4caf19a77cf544e5d883e72801426ddbb79ba0c5
709889220@qq.com <709889220tm>
2018-12-17 14:29:43 +0800

    Initial Commit

---
+++ KeepView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeepView
+//
+//  Created by maple on 2018/12/17.
+//  Copyright © 2018年 Xuyongkang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
